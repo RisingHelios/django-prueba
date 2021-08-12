1424e248d347820a67f025363b69081db6f448e5
Cristian Esteban Neira Gallego <ceneira5@misena.edu.co>
2021-08-11 21:22:52 -0500

    project django prueba 2

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    email = models.EmailField(max_length=255)
+
+def __str__(self):
+	return self.nombre
+
+class Producto(models.Model):
+    nombre= models.CharField(max_length=50)
--- home/urls.py
+++ home/urls.py
@@ -1,1 +1,7 @@
-urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+ path('',views.index, name='index'),
+ path('estudiantes/<str:pk>/', views.estudiantes, name='estudiantes'),
+]
